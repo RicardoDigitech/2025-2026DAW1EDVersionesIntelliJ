907dee79106f5291ef0eeba03723a77595aaa824
RicardoDigitech <ricardo.sornosa@digitechfp.com>
2026-01-28 13:10:18 +0100

    Nueva función.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola.");
         System.out.println("¿Qué tal?");
+        System.out.println("Soy una nueva función.");
     }
 }
